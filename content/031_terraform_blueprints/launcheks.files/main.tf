e44c30011d350276ec23f0da351e94a1dec23a28
Walker Miller <walker.miller@gmail.com>
2022-05-11 18:13:56 -0400

    commenting out all but the first compute option to speed up

--- content/031_terraform_blueprints/launcheks.files/main.tf
+++ content/031_terraform_blueprints/launcheks.files/main.tf
@@ -123,55 +123,55 @@ module "eks_blueprints" {
       subnet_ids      = module.aws_vpc.private_subnets
      bottlerocket     = true
     }
-    mg_bottlerocket_x86 = {
-      node_group_name = "managed-bottlerocket"
-      instance_types  = ["m5.large"]
-      min_size        = "2"
-      subnet_ids      = module.aws_vpc.private_subnets
-      ami_type        = "BOTTLEROCKET_x86_64"
-      release_version = ""
-      k8s_labels ={
-        Environment = "preprod"
-        Zone        = "dev"
-        env         = "BOTTLEROCKET_x86"
-      }
+  #   mg_bottlerocket_x86 = {
+  #     node_group_name = "managed-bottlerocket"
+  #     instance_types  = ["m5.large"]
+  #     min_size        = "2"
+  #     subnet_ids      = module.aws_vpc.private_subnets
+  #     ami_type        = "BOTTLEROCKET_x86_64"
+  #     release_version = ""
+  #     k8s_labels ={
+  #       Environment = "preprod"
+  #       Zone        = "dev"
+  #       env         = "BOTTLEROCKET_x86"
+  #     }
       
-    }
-    mg_bottlerocket_arm = {
-      node_group_name = "managed-bottlerocket-arm"
-      instance_types  = ["m6g.large"]
-      min_size        = "2"
-      subnet_ids      = module.aws_vpc.private_subnets
-      ami_type        = "BOTTLEROCKET_ARM_64"
-      release_version = ""
-      k8s_labels ={
-        Environment = "preprod"
-        Zone        = "dev"
-        env         = "BOTTLEROCKET_ARM"
-      }
+  #   }
+  #   mg_bottlerocket_arm = {
+  #     node_group_name = "managed-bottlerocket-arm"
+  #     instance_types  = ["m6g.large"]
+  #     min_size        = "2"
+  #     subnet_ids      = module.aws_vpc.private_subnets
+  #     ami_type        = "BOTTLEROCKET_ARM_64"
+  #     release_version = ""
+  #     k8s_labels ={
+  #       Environment = "preprod"
+  #       Zone        = "dev"
+  #       env         = "BOTTLEROCKET_ARM"
+  #     }
       
-    }
+  #   }
 
-  }
+  # }
 
-  fargate_profiles = {
-    default = {
-      fargate_profile_name = "default"
-      fargate_profile_namespaces = [
-        {namespace = "default" 
-        k8s_labels = {} }, 
-        {namespace = "nginx-fargate"
-        k8s_labels = {} }
-        ]
+#   fargate_profiles = {
+#     default = {
+#       fargate_profile_name = "default"
+#       fargate_profile_namespaces = [
+#         {namespace = "default" 
+#         k8s_labels = {} }, 
+#         {namespace = "nginx-fargate"
+#         k8s_labels = {} }
+#         ]
 
-      subnet_ids = module.aws_vpc.private_subnets
+#       subnet_ids = module.aws_vpc.private_subnets
 
-      additional_tags = {
-        ExtraTag = "Fargate"
-      }
-    },
-  }
-}
+#       additional_tags = {
+#         ExtraTag = "Fargate"
+#       }
+#     },
+#   }
+# }
 
 module "eks_blueprints_kubernetes_addons" {
   source = "../../../../terraform-aws-eks-blueprints/modules/kubernetes-addons"
